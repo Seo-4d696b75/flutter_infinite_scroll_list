7085eb55934fe209226c4fc042c89c0f3a24a00e
k_senda <k_senda@yumemi.co.jp>
2023-07-26 22:26:21 +0900

    fix: error, empty page

--- lib/ui/search/page/empty_result_page.dart
+++ lib/ui/search/page/empty_result_page.dart
@@ -7,10 +7,12 @@ class EmptyResultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Center(
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           Icon(
             Icons.search_off_outlined,
             color: Colors.grey,
+            size: 80,
           ),
           SizedBox(height: 16),
           Text('No result found'),
--- lib/ui/search/page/error_page.dart
+++ lib/ui/search/page/error_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_infinite_scroll_list/ui/search/search_result.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ErrorPage extends ConsumerWidget {
@@ -8,17 +9,19 @@ class ErrorPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Center(
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           const Icon(
             Icons.error_outline,
             color: Colors.red,
+            size: 80,
           ),
           const SizedBox(height: 16),
           const Text('An Error happened'),
           const SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
-              // TODO call reload operation
+              ref.read(searchResultProvider.notifier).reload();
             },
             child: const Text('Retry'),
           ),
